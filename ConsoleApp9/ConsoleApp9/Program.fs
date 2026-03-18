a78ce29053b006b1f614a7e364c5f75ac52a28b3
Pank-cake <od.uvanchik@yandex.ru>
2026-03-18 11:34:22 +0500

    Update Program.fs

--- ConsoleApp9/ConsoleApp9/Program.fs
+++ ConsoleApp9/ConsoleApp9/Program.fs
@@ -1,86 +1,80 @@
-﻿// Задание 1: Вариант - 9
 open System
 
 // Тип дерева
-type Leaf = { value: float; left: Leaf option; right: Leaf option }
+type Tree<'T> =
+    | Node of 'T * Tree<'T> * Tree<'T>
+    | Nil
 
-// Рекурсивная вставка значения в бинарное дерево поиска
-let rec insertTree (tree: Leaf option) (newValue: float) : Leaf =
+// Вставка
+let rec insertTree tree newValue =
     match tree with
-    | Some t ->
-        if newValue <= t.value then
-            { t with left = Some (insertTree t.left newValue) } // Вставка в левое поддерево
-        else 
-            { t with right = Some (insertTree t.right newValue) } // Вставка в правое поддерево
-    | None -> { value = newValue; left = None; right = None }
+    | Node(value, left, right) ->
+        if newValue <= value then
+            Node(value, insertTree left newValue, right)
+        else
+            Node(value, left, insertTree right newValue)
+    | Nil -> Node(newValue, Nil, Nil)
 
-// Запись значений дерева в список
-let rec reWriteList (tree: Leaf option) : float list =
-    match tree with
-    | None -> 
-        []
-    | Some t ->
-        let leftValues = reWriteList t.left
-        let rootValue = [t.value]
-        let rightValues = reWriteList t.right
-        let allValues = leftValues @ rootValue @ rightValues // Объединение списков 
-        allValues
-
-
-// Вывод дерева
-let rec printTree t =
-    match t with
-    | Some node ->
-        printTree node.left
-        printf "%f " node.value
-        printTree node.right
-    | None -> ()
-
-// Проверка на корректность ввода числа
+// Проверка ввода
 let rec checkQuantity() =
     let input = Console.ReadLine()
     let suc, n = Int32.TryParse(input)
     if suc && n > 0 then n
     else
-        printfn "Введите положительное целое число "
+        printfn "Введите положительное целое число:"
         checkQuantity()
 
-// Генерация случайного дерева со значениями [-10..10]
-let generateRandomTree n =
-    let rand = Random()
-    let values =
-        List.init n (fun _ -> 
-            let number = rand.NextDouble()
-            number * 20.0 - 10.0
-        )
-    List.fold (fun acc v -> Some (insertTree acc v)) None values
+// Генерация дерева случайных чисел
+let generateTree n =
+    let r = Random()
+    let rec loop i tree =
+        if i = 0 then tree
+        else
+            let value = r.NextDouble() * 20.0 - 10.0
+            loop (i - 1) (insertTree tree value)
+    loop n Nil
 
-// Построение нового дерева
-let buildTree (values: float list) =
-    List.fold (fun acc v -> Some (insertTree acc v)) None values
+// TreeMap
+let rec treeMap f tree =
+    match tree with
+    | Nil -> Nil
+    | Node(value, left, right) ->
+        Node(f value, treeMap f left, treeMap f right)
+
+// Определяет обход
+let infix root left right = (right(); root(); left())
+
+let travDepth strategy action tree =
+    let rec walk curTree depth =
+        match curTree with
+        | Node (value, left, right) -> 
+            strategy (fun () -> action value depth)
+                     (fun () -> walk left (depth + 4))
+                     (fun () -> walk right (depth + 4))
+        | Nil -> ()
+    walk tree 0
+
+let spaces n = String.replicate n "  "
+
+// Главная функция вывода
+let printTree T = 
+    travDepth infix (fun x dep -> printfn "%s%A" (spaces dep) x) T
 
 [<EntryPoint>]
 let main args =
-    printfn "Количество элементов дерева: "
+    printf "Количество элементов: "
     let n = checkQuantity()
 
-    let tree = generateRandomTree n
-    printf "Исходное дерево: "
-    printTree tree
-    let listValues = reWriteList tree
-    let newValues =
-        listValues
-        |> List.map (fun x ->
-            if x < 0.0 then
-                0.0
-            else
-                1.0
-        )
+    let Tree1 = generateTree n
 
+    printfn "\nИсходное дерево:"
+    printTree Tree1
 
-    let newTree = buildTree newValues
+    let Tree2 =
+        treeMap (fun x -> if x < 0.0 then 0 else 1) Tree1
 
-    printf "\nНовое дерево: "
-    printTree newTree
+    printfn "\nНовое дерево:"
+    printTree Tree2
 
     0
+
